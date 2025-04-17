664c0c7fc5fc726f9065641881f1e62d88000599
White <emmanuel.white@amalitechtraining.org>
2025-04-17 11:04:37 +0000

    modified the combined script. Added few comments

--- week3/inventory_system/sql_scripts/combined_script.sql
+++ week3/inventory_system/sql_scripts/combined_script.sql
@@ -1,8 +1,9 @@
 -- =====================================================
--- PHASE 1: DROP TABLES & CREATE SCHEMA
+-- 1. DROP TABLES & CREATE SCHEMA
 -- =====================================================
 
 -- Drop all tables if they already exist to reset the schema (use with caution)
+-- DON'T RUN IN PRODUCTION
 DROP TABLE IF EXISTS inventory_logs, order_details, orders, products, customers;
 
 -- Create table to store customer information
@@ -76,7 +77,7 @@ VALUES
   ('Laptop Bag', 'Accessories', 75.00, 50, 10, 'HP Laptop bag for size 15"6 laptops. (Color: Gray)' );
 
 -- =====================================================
--- PHASE 2: Create Order Placement Logic
+-- 2. Create Order Processing Function 
 -- =====================================================
 
 -- Create function to place an order
@@ -87,68 +88,78 @@ VALUES
 -- 4. Logs the inventory change
 
 CREATE OR REPLACE FUNCTION place_order(
-    p_customer_id INT,
-    p_status VARCHAR,
-    p_product_ids INT[],
-    p_quantities INT[]
+    p_customer_id INT,					-- It takes the customer id
+    p_status VARCHAR,					-- the order's status
+    p_product_ids INT[],				-- takes an array of product ids
+    p_quantities INT[]					-- quantities of products ordered
 )
 RETURNS VOID AS $$
-DECLARE
-    v_order_id INT;
-    v_total_amount NUMERIC(10, 2) := 0;
-    i INT;
-    v_unit_price NUMERIC(10, 2);
-    v_total_price NUMERIC(10, 2);
-    v_discount NUMERIC(10, 2) := 0;
+DECLARE											-- declare local variables inside the function for processing
+    v_order_id INT;								-- create a local variable from the new id from order
+    v_total_amount NUMERIC(10, 2) := 0;		-- total amount of the order
+    i INT;										-- a counter for looping
+    v_unit_price NUMERIC(10, 2);				-- variable for unit price
+    v_total_price NUMERIC(10, 2);				-- variable for storing total price
+    v_discount NUMERIC(10, 2) := 0;			-- stores the discount calculated
 BEGIN
-    -- Insert new order
+    -- Insert a new order
+	/*
+		A new order is created, and the order is saved into the local variable v_order_id
+	*/
     INSERT INTO orders (customer_id, order_date, total_amount, status)
     VALUES (p_customer_id, CURRENT_DATE, 0, p_status)
     RETURNING order_id INTO v_order_id;
 
     -- Process each product in the order
-    FOR i IN 1 .. array_length(p_product_ids, 1) LOOP
-        SELECT price INTO v_unit_price FROM products WHERE product_id = p_product_ids[i];
+    FOR i IN 1 .. array_length(p_product_ids, 1) LOOP			-- Loop through all the product ids
+        SELECT price INTO v_unit_price FROM products WHERE product_id = p_product_ids[i]; -- get the unit price of the product
 
-        v_total_price := v_unit_price * p_quantities[i];
+        v_total_price := v_unit_price * p_quantities[i];		-- calculates the price before discount
 
-        -- Apply discount logic
-        IF p_quantities[i] >= 20 THEN
+        -- Apply discount 
+        IF p_quantities[i] >= 20 THEN					-- if order quantity is 20 or more give 15% discount
             v_discount := v_total_price * 0.15;
         ELSIF p_quantities[i] >= 10 THEN
-            v_discount := v_total_price * 0.07;
+            v_discount := v_total_price * 0.07;		-- if order quantity is 10 or more, but less than 20 give 7% discount
         ELSE
-            v_discount := 0;
+            v_discount := 0;							-- give no discounts if order is less than 10
         END IF;
 
-        -- Subtract discount
+        -- Subtract discount from total price
         v_total_price := v_total_price - v_discount;
 
-        -- Insert order details
+        -- Insert each order into the order details table
         INSERT INTO order_details (order_id, product_id, quantity, unit_price, total_amount)
         VALUES (v_order_id, p_product_ids[i], p_quantities[i], v_unit_price, v_total_price);
 
-        -- Update stock
+        -- Update the product stock after
         UPDATE products
         SET stock_quantity = stock_quantity - p_quantities[i]
         WHERE product_id = p_product_ids[i];
 
-        -- Log inventory change
+        -- Log the change into inventory
         INSERT INTO inventory_logs (product_id, change_quantity, change_type)
         VALUES (p_product_ids[i], -p_quantities[i], 'sale');
 
-        -- Accumulate total
+        -- Accumulate the total order cost
         v_total_amount := v_total_amount + v_total_price;
     END LOOP;
 
-    -- Update order total
+    -- Update order with the final total
     UPDATE orders SET total_amount = v_total_amount WHERE order_id = v_order_id;
-
+	
+	-- show a message confirming order: displays the order id and total
     RAISE NOTICE 'Order % placed successfully with total $%s after discount', v_order_id, v_total_amount;
 END;
 $$ LANGUAGE plpgsql;
 
--- Create trigger function to log inventory changes after update
+
+-- Create trigger function to log inventory changes 
+/*
+	This trigger function runs when the products stock is manually updated, not by the place order function.
+	It logs the product id, how much the stock changed, the time of updating, and that the change was manual
+	** in an upgrade, staff who made changes manually will be added.
+*/
 CREATE OR REPLACE FUNCTION log_inventory_change() 
 RETURNS TRIGGER AS $$
 BEGIN
@@ -166,10 +177,16 @@ FOR EACH ROW
 EXECUTE FUNCTION log_inventory_change();
 
 -- =====================================================
--- PHASE 3: Create Views for Analysis
+-- 3. Create Views for Analysis
 -- =====================================================
 
 -- View to show customer order summaries
+/*
+	* This VIEW joins customers, orders, and order_details tables
+	* Counts how many products were ordered per order
+	* Groups the data by customer and order
+	* Sorts the results from the newest to the oldest
+*/
 CREATE OR REPLACE VIEW customer_order_summary AS
 SELECT 
     c.customer_id,
@@ -185,6 +202,10 @@ GROUP BY c.customer_id, c.customer_name, o.order_id, o.order_date, o.total_amoun
 ORDER BY o.order_date DESC;
 
 -- View to list low stock products
+/*
+	This view filters products that have less stock than their reorder level.
+	It calculates the shortage amount.
+*/
 CREATE OR REPLACE VIEW low_stock_report AS
 SELECT 
     product_id,
@@ -196,6 +217,13 @@ FROM products
 WHERE stock_quantity < reorder_level;
 
 -- View to show customer spending levels
+/*
+	This VIEW segments customers based on their spending habits. 
+	It assigns a spending tier to each customer
+	* GOLD for customers who spend more than 500 
+	* SILVER for customers who spend between 100 and 500
+	* BRONZE for customers who spend less than 100
+*/
 CREATE OR REPLACE VIEW customer_spending_tier AS
 SELECT 
     c.customer_id,
@@ -211,16 +239,16 @@ JOIN orders o ON c.customer_id = o.customer_id
 GROUP BY c.customer_id, c.customer_name;
 
 -- =====================================================
--- PHASE 4: Stock Replenishment Procedure
+-- 4. Stock Replenishment Procedure
 -- =====================================================
 
--- Procedure to automatically replenish stock
+-- Procedure to automatically replenish stock when the product is below the reorder level
 CREATE OR REPLACE PROCEDURE replenish_stock()
 LANGUAGE plpgsql
 AS $$
 DECLARE
-    p RECORD;
-    qty_to_add INT;
+    p RECORD;				-- p is a variable that will hold one row(RECORD) of the from the products table at a time.
+    qty_to_add INT;			-- stores how much quantity of items to be added
 BEGIN
     -- Loop through products that are low in stock
     FOR p IN 
@@ -228,8 +256,8 @@ BEGIN
         FROM products 
         WHERE stock_quantity < reorder_level
     LOOP
-        -- Calculate how much stock to add to reach 125% of reorder level
-        qty_to_add := CEIL((p.reorder_level * 1.25) - p.stock_quantity);
+        -- Calculate how much stock to add to reach 100% of reorder level
+        qty_to_add := CEIL((p.reorder_level * 1.00) - p.stock_quantity);
 
         -- Update product stock
         UPDATE products 
@@ -240,6 +268,7 @@ BEGIN
         INSERT INTO inventory_logs (product_id, change_quantity, change_type)
         VALUES (p.product_id, qty_to_add, 'replenishment');
 
+		-- notifies which products were restocked and by how much
         RAISE NOTICE 'Replenished product % with quantity %', p.product_id, qty_to_add;
     END LOOP;
 END;
